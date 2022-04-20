a55ed467594a70ef93a685fa5d0936e85d487034
shacoo <shacoo@xuexi.cn>
2022-04-20 14:40:39 +0800

    完成了对商品列表页面的开发

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
 	<view>
-		goods_list
+		<view class="goods-list">
+			<view v-for="(goods, i) in goodsList" :key="i" @click="gotoDetail(goods)">
+				<my-goods :goods="goods"></my-goods>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,8 +12,60 @@
 	export default {
 		data() {
 			return {
-				
+				// 请求参数对象
+				queryObj: {
+					query: '',
+					cid: '',
+					pagenum: 1,
+					pagesize: 10
+				},
+				goodsList: [],
+				total: 0,
+				isloading: false
 			};
+		},
+		onLoad(options) {
+			this.queryObj.query = options.query || ''
+			this.queryObj.cid = options.cid || ''
+
+			// console.log(this.queryObj);
+			this.getGoodsList()
+		},
+		methods: {
+			// 发起请求获取商品列表
+			async getGoodsList(cb) {
+				this.isloading = true
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+				this.isloading = false
+				cb && cb()
+				if (res.meta.status !== 200) return uni.$showMsg()
+
+				this.goodsList = [...this.goodsList, ...res.message.goods]
+				this.total = res.message.total
+			},
+			// 点击跳转到商品详情页面
+			gotoDetail(goods) {
+				uni.navigateTo({
+					url: '/subpkg/goods_detail/goods_detail?id=' + goods.goods_id
+				})
+			}
+		},
+		onReachBottom() {
+			if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕')
+			if (this.isloading) return
+			this.queryObj.pagenum++
+			this.getGoodsList()
+		},
+		onPullDownRefresh() {
+			// 重置数据
+			this.queryObj.pagenum = 1
+			this.total = 0
+			this.isloading = false
+			this.goodsList = []
+			// 重新发起请求
+			this.getGoodsList(() => uni.stopPullDownRefresh())
 		}
 	}
 </script>
